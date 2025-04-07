c227b82714e66ef39a053a7d026b16922034f803
NSYL\12968 <1296853739@qq.com>
2025-04-07 14:19:18 +0800

    fix

--- common/message_definition.go
+++ common/message_definition.go
@@ -18,12 +18,13 @@ const (
 	ReliableMsg    //可靠消息
 	ReliableMsgAck //可靠消息回执
 	GossipMsg      //gossip消息
-	UnicastMsg
 	//plumtree
 	EagerPush //急迫推送
 	LazyPush  //发送IHAVE
 	Graft     //收到IHAVE之后，发现自己没有，所以进行主动的拉取
 	Prune     //用于修剪，自己已经收到了EagerPush
+
+	UnicastMsg
 )
 
 type MsgAction = byte
