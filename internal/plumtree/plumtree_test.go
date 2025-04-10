1f2defa06274207d1d39af00e2523af4ab5a7f73
lingkong214 <921386509@qq.com>
2025-04-10 21:20:12 +0800

    test plumtree by net

--- internal/plumtree/plumtree_test.go
+++ internal/plumtree/plumtree_test.go
@@ -1,21 +1,78 @@
 package plumtree
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"math/rand/v2"
-	"net/http"
-	"net/url"
+	"net"
 	"testing"
 	"time"
 )
 
-type PlumServer struct {
-	member  map[string]int
-	message map[string]int
-}
+type Command = byte
+
+const (
+	Stop Command = iota
+)
 
 var pServerMap map[string]PlumServer
+
+type PlumServer struct {
+	host     string
+	listener net.Listener
+	member   map[string]int
+	message  map[string]int
+}
+
+func (server *PlumServer) init(port int) {
+	server.host = fmt.Sprintf("127.0.0.1:%d", port)
+	server.member = map[string]int{}
+	server.member[server.host] = 1
+}
+
+func (server *PlumServer) start() {
+	listener, err := net.Listen("tcp", server.host)
+	if err != nil {
+		log.Println("linten err", err)
+		return
+	}
+	server.listener = listener
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("acccept err", err)
+			break
+		}
+
+		go server.process(conn)
+	}
+}
+
+func (server *PlumServer) stop() {
+	server.listener.Close()
+}
+
+func (server *PlumServer) process(conn net.Conn) {
+	defer conn.Close()
+	for {
+		reader := bufio.NewReader(conn)
+		buf := make([]byte, 128)
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			log.Println("read err", err)
+			break
+		}
+		res := string(buf[:n])
+		log.Println(res)
+
+		conn.Write([]byte("success"))
+
+		if buf[0] == Stop {
+			server.stop()
+		}
+	}
+}
 
 func randomNum(count int, fanout int) []int {
 	numMap := make(map[int]int)
@@ -59,57 +116,44 @@ func nextNodes(host string) []string {
 	return tAddr
 }
 
-func plumBroadcast(host string, route *url.URL) {
-	nodes := nextNodes(host)
-	for _, node := range nodes {
-		http.Get("http://" + node + route.RequestURI())
+func TestMessageClient(t *testing.T) {
+	conn, err := net.Dial("tcp", "127.0.0.1:40000")
+	if err != nil {
+		log.Println("dial err", err)
+		return
 	}
+	defer conn.Close()
+
+	// _, err = conn.Write([]byte("hello world"))
+	_, err = conn.Write([]byte{Stop})
+	if err != nil {
+		log.Println("write err", err)
+		return
+	}
+
+	buf := [512]byte{}
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		log.Println("read err", err)
+		return
+	}
+	fmt.Println(string(buf[:n]))
 }
 
-func sendMessage(w http.ResponseWriter, r *http.Request) {
-	host := r.Host
-	log.Println(host, "sendMessage")
-	msg := r.URL.Query().Get("msg")
-	r.URL.RequestURI()
-	if _, exisit := pServerMap[host].message[msg]; !exisit {
-		pServerMap[host].message[msg] = 1
-		plumBroadcast(host, r.URL)
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("success"))
-}
-
-func TestServer(t *testing.T) {
-	pServerMap = make(map[string]PlumServer)
-	pMember := make(map[string]int)
+func TestMessageServer(t *testing.T) {
+	serverArr := make([]PlumServer, 0)
 	for i := range 5 {
-		lAttr := fmt.Sprintf("localhost:%d", 40000+i)
-		pMember[lAttr] = 1
+		server := new(PlumServer)
+		server.init(40000 + i)
+		serverArr = append(serverArr, *server)
 	}
 
-	for k, _ := range pMember {
-		pServerMap[k] = PlumServer{member: pMember, message: make(map[string]int)}
-	}
-
-	http.HandleFunc("/applyLeave", applyLeave)
-	http.HandleFunc("/sendMessage", sendMessage)
-	for k, _ := range pServerMap {
-		go startServer(k)
+	for _, server := range serverArr {
+		go server.start()
 	}
 
 	for {
 		time.Sleep(1 * time.Second)
 		time.Sleep(1 * time.Second)
 	}
-}
-
-func startServer(lAttr string) {
-	log.Println("http start ", lAttr)
-	if err := http.ListenAndServe(lAttr, nil); err != nil {
-		log.Printf("Error starting server: %s\n", err)
-	}
-}
-
-func applyLeave(w http.ResponseWriter, r *http.Request) {
-
 }
